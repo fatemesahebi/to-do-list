17816c70c81e01edd6fe631e83361e6ce20dce5e
fateme <f.sahebi.fs@gmail.com>
2021-11-18 13:23:36 +0330

    add night and day theme to cards

--- javascript.js
+++ javascript.js
@@ -1,16 +1,19 @@
 // let input=document.getElementById('input')
-let listText= document.getElementsByClassName('theme-text')
+let listText= document.getElementsByClassName('list-item-contain')
+let listBox=document.getElementsByClassName('list-item')
 let circle=document.getElementsByClassName('circle')
 let close=document.getElementsByClassName('close')
 let list=document.getElementById('list--items')
 let moon=document.getElementById('moon')
 let sun=document.getElementById('sun')
-let main=document.getElementsByClassName('main')
+let main=document.getElementsByClassName('main')[0]
 let theme=document.getElementsByClassName('theme')
-let themeSvg=document.getElementsByClassName('theme-svg')
+let themeSvg=document.getElementsByClassName('theme-svg')[0]
 let activeFilter=document.getElementById('active')
 let completeFilter=document.getElementById('complete')
 let all=document.getElementById('all')
+let activeText=document.getElementById('acitveText')
+let clear=document.getElementById('clear')
 
 let toDolist = [
 {title:"Complete online JavaScript course",
@@ -22,14 +25,17 @@ complete:false},
     {title:"Read for 1 hour",
         complete:false}
 ]
+function activeNum(){
+    let activeNum=toDolist.filter(item=>item['complete']===false).length
+    activeText.innerHTML=`${[activeNum]} items left`
+}
 
-let acivenum=toDolist.filter(item=>item['complete']===false).length
 
 // define function for creat html for to do list
 function ToDoHtml(obj){
     return `
       <div class="list-item theme-position">
-            <div class="theme-text">
+            <div class="theme-text list-item-contain">
             <div class="circle"></div>
             <p>${obj['title']}</p>
                 </div>
@@ -43,11 +49,11 @@ function addClass(arr){
     for (let i=0;i<arr.length;i++){
         if (arr[i]['complete']) {
             listText[i].classList.add('text')
-            circle[i].classList.add('circle-select')
+            circle[i+1].classList.add('circle-select')
         }
         else{
             listText[i].classList.remove('text')
-            circle[i].classList.remove('circle-select')
+            circle[i+1].classList.remove('circle-select')
         }
     }
 }
@@ -57,12 +63,18 @@ function creatHtmls(arr) {
         return ToDoHtml(card)
     }).join('')
 }
+list.innerHTML = creatHtmls(toDolist)
+addClass(toDolist)
+complete(toDolist)
+closelist(toDolist)
+activeNum()
 
 function complete(arr){
-    for(let i=1;i<=arr.length;i++){
+    for(let i=0;i<listText.length;i++){
         listText[i].onclick=function (){
             arr[i]['complete'] = !arr[i]['complete'];
             addClass(toDolist)
+            activeNum()
         }
     }
 }
@@ -74,14 +86,12 @@ function closelist(arr){
             addClass(arr)
             complete(arr)
             closelist(arr)
+            activeNum()
         }
     }
 }
 
-list.innerHTML = creatHtmls(toDolist)
-addClass(toDolist)
-complete(toDolist)
-closelist(toDolist)
+
 
 
 //********************
@@ -92,16 +102,33 @@ themeSvg.onclick = function (){
     if(moon.style.display==='inline-block'){
         moon.style.display='none'
         sun.style.display='inline-block'
-        main.style.backgroundColor='black'
-        theme.style.backgroundColor='#4d5066;'
-        console.log('**')
+        main.style.backgroundColor='#161722'
+        theme[0].style.backgroundColor='#252736'
+        theme[1].style.backgroundColor='#252736'
+        document.getElementsByClassName('list')[0].style.boxShadow='2px 16px 25px #1F242AFF'
+        main.style.backgroundImage='url("image/bg-desktop-dark.73e47dbb.jpg")'
+        // document.querySelector(':root').style.setProperty(' --filter-hover-color','#dcd9d9')
+        document.documentElement.style.setProperty(' --filter-hover-color','#dcd9d9')
+
+
+
     }
     else{
         moon.style.display='inline-block'
         sun.style.display='none'
         main.style.backgroundColor='white'
-        theme.style.backgroundColor='white;'
-        console.log('&&')
+        theme[0].style.backgroundColor='white'
+        theme[1].style.backgroundColor='white'
+        document.getElementsByClassName('list')[0].style.boxShadow='2px 16px 25px lightgray'
+        main.style.backgroundImage='url("image/bg-desktop-light.3508d620.jpg")'
+        // document.querySelector(':root').style.setProperty(' --filter-hover-color','#3d3d3d')
+        document.documentElement.style.setProperty(' --filter-hover-color','#3d3d3d')
+
+
+
+
+
+
     }
 }
 
@@ -114,11 +141,13 @@ document.getElementById('input').addEventListener('keypress',(event) => {
         addClass(toDolist)
         complete(toDolist)
         closelist(toDolist)
+        activeNum()
+
 
     }
 })
 completeFilter.onclick=function (){
-    completeFilter.style.color='blue'
+    completeFilter.style.color='var(--blue)'
     activeFilter.style.color='inherit'
     all.style.color='inherit'
   let completeList= toDolist.filter(item=>item['complete']===true)
@@ -127,20 +156,23 @@ completeFilter.onclick=function (){
     addClass(completeList)
     complete(completeList)
     closelist(completeList)
+    activeNum()
+
 }
 
 activeFilter.onclick=function (){
-    activeFilter.style.color='blue'
+    activeFilter.style.color='var(--blue)'
     all.style.color='inherit'
     completeFilter.style.color='inherit'
     let activeList= toDolist.filter(item=>item['complete']===false)
     list.innerHTML = creatHtmls(activeList)
     addClass(activeList)
     complete(activeList)
+    activeNum()
     closelist(activeList)
 }
 all.onclick=function (){
-    all.style.color='blue'
+    all.style.color='var(--blue)'
     activeFilter.style.color='inherit'
     completeFilter.style.color='inherit'
 
@@ -148,4 +180,14 @@ all.onclick=function (){
     addClass(toDolist)
     complete(toDolist)
     closelist(toDolist)
+    activeNum()
 }
+clear.onclick=function (){
+   toDolist=toDolist.filter(item=>item['complete']===false)
+    list.innerHTML = creatHtmls(toDolist)
+    addClass(toDolist)
+    complete(toDolist)
+    closelist(toDolist)
+    activeNum()
+}
+
